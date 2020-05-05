2ef15ad5e9b4ffb57b0486601104bd9739f168c9
Tarun Mangukiya <tarunmangukiya@hotmail.com>
2020-05-05 11:17:55 +0530

    Release: v1.0.0

---
+++ icons/uil-comment-add.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Svg, { Path } from "react-native-svg";
+import PropTypes from "prop-types";
+
+const UilCommentAdd = props => {
+  const { color, size, ...otherProps } = props;
+  return (
+    <Svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill={color}
+      {...otherProps}
+    >
+      <Path d="M5,8A1,1,0,0,0,7,8V7H8A1,1,0,0,0,8,5H7V4A1,1,0,0,0,5,4V5H4A1,1,0,0,0,4,7H5ZM18,5H12a1,1,0,0,0,0,2h6a1,1,0,0,1,1,1v9.72l-1.57-1.45a1,1,0,0,0-.68-.27H8a1,1,0,0,1-1-1V12a1,1,0,0,0-2,0v3a3,3,0,0,0,3,3h8.36l3,2.73A1,1,0,0,0,20,21a1.1,1.1,0,0,0,.4-.08A1,1,0,0,0,21,20V8A3,3,0,0,0,18,5Z" />
+    </Svg>
+  );
+};
+
+UilCommentAdd.propTypes = {
+  color: PropTypes.string,
+  size: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+};
+
+UilCommentAdd.defaultProps = {
+  color: "currentColor",
+  size: "24"
+};
+
+export default UilCommentAdd;
